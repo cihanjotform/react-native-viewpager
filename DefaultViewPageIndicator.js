ae5ca583e5a4e0dd1da534fcc2b4028d4e01c5af
zqg <269695882@qq.com>
2018-02-08 11:18:28 +0800

    修复 bug

--- DefaultViewPageIndicator.js
+++ DefaultViewPageIndicator.js
@@ -10,7 +10,7 @@ var {
   View,
   Animated,
 } = ReactNative;
-
+var PropTypes = require('prop-types');
 var deviceWidth = Dimensions.get('window').width;
 var DOT_SIZE = 6;
 var DOT_SAPCE = 4;
@@ -48,9 +48,9 @@ var styles = StyleSheet.create({
 
 var DefaultViewPageIndicator = React.createClass({
   propTypes: {
-    goToPage: React.PropTypes.func,
-    activePage: React.PropTypes.number,
-    pageCount: React.PropTypes.number
+    goToPage: PropTypes.func,
+    activePage: PropTypes.number,
+    pageCount: PropTypes.number
   },
 
   getInitialState() {
